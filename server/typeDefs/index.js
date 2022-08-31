e2ea2d8a3be35cdbea4c7de72a6a884a081c92bd
Faisal Mohammed <m.faisal244@gmail.com>
2022-08-31 14:49:46 +0100

    Defined all GraphQL mutations and queries

--- server/typeDefs/index.js
+++ server/typeDefs/index.js
@@ -1,39 +1,58 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-type Book {
-  authors: [String]
-  description: String!
-  bookId: String!
-  image: String
-  link: String
-  title: String!
-}
-type User {
-  id:ID!
-  username: String!
-  email: String!
-  password: String!
-  bookCount: Int!
-  savedBooks: [Book]
-}
+	type Book {
+		authors: [String]
+		description: String!
+		bookId: String!
+		image: String
+		link: String
+		title: String!
+	}
+	type User {
+		id: ID!
+		username: String!
+		email: String!
+		password: String!
+		bookCount: Int!
+		savedBooks: [Book]
+	}
 
-type SignupResponse {
-  token: string
-  user: User
-}
+	type AuthResponse {
+		token: string!
+		user: User!
+	}
 
-input SingupInput {
-  username: String!
-  email: String!
-  password: String!
-  
+	input SingupInput {
+		username: String!
+		email: String!
+		password: String!
+	}
 
-}
+	input loginInput {
+		email: String!
+		password: String!
+	}
 
-mutation{
-  signup(input:SingupInput!): SignupResponse
-}
+	input SaveBookInput {
+		bookId: String!
+		authors: [String]
+		image: String
+		title: String
+		description: String
+		link: String
+	}
+
+	type Query {
+		me: User!
+	}
+
+	type mutation {
+		signup(input: SingupInput!): AuthResponse
+		login(input: loginInput!): AuthResponse
+		saveBook(input: SaveBookInput!): User
+		removeBook(bookId: String!): User
+	}
 `;
 
 module.exports = typeDefs;
